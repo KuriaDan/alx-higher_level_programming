0a1611c23377dd0a4dd2dbaea83ca300a53bc130
KuriaDan <thuku.kuria@live.com>
2022-09-20 17:17:43 +0300

    combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 8):
     for j in range(0, 10):
-        if j <= 1:
+        if j <= i:
             continue
         else:
             print("{}{}".format(i, j), end=", ")
