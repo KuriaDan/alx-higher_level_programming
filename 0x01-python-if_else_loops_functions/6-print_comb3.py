c94a4601a1e28adec4b7ac31da11b6f5b02d585e
KuriaDan <thuku.kuria@live.com>
2022-09-20 17:14:28 +0300

    combo

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 8):
+    for j in range(0, 10):
+        if j <= 1:
+            continue
+        else:
+            print("{}{}".format(i, j), end=", ")
+print('89')
