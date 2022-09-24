2fc98b3468542719917ef5cb630f64adbc379dae
KuriaDan <thuku.kuria@live.com>
2022-09-24 15:08:04 +0300

    remove c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for element in my_string:
-        if element in ['c', 'C']:
-            continue
-        else:
-            print(element)
+    return (my_string.translate({ord(i): None for i in 'cC'}))
