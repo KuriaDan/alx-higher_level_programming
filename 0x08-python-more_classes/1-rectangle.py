58cba6d6c66fc84891b8b85e561c302e279d15ba
KuriaDan <thuku.kuria@live.com>
2022-10-17 22:39:59 +0300

    real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represents a rectangle"""
 
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         """Initialize a new rectangle:
         args:
             width (int): width of the rectangle
