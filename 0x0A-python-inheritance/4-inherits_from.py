666d3acec551600d68f681f9b9ff2901607269cd
KuriaDan <thuku.kuria@live.com>
2022-10-24 22:11:04 +0300

    only subclass of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""
+function that returns True if the object is an
+instance of a class that inherited (directly or
+indirectly) from the specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns True or False"""
+
+    return isinstance(obj, a_class) and type(obj) != a_class
