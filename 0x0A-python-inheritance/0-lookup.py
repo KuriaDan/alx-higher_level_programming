8dccc0c292bcdcf113cd9577a0108a47f7f1080b
KuriaDan <thuku.kuria@live.com>
2022-10-24 19:56:38 +0300

    attributes of a python object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Function that returns a list of available
+attributes and methods of an object"""
+
+
 def lookup(obj):
+    """
+    Function: lookup()
+    returns a list objectd
+    """
+
     return list(dir(obj))
