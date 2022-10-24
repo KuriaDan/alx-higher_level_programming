c1551f2c62a2f948e4131cbc51a9ecdb3f89541d
KuriaDan <thuku.kuria@live.com>
2022-10-24 23:09:39 +0300

    improved geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+An empty Basegeometry class for
+"""
+
+
+class BaseGeometry:
+    """Returns BaseGeometry class"""
+    def area(self):
+        """ raises an exception"""
+        raise Exception('area() is not implemented')
