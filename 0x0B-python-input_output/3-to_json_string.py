1051eb090df882b59d3b89aa9b85c76640a4c1d5
KuriaDan <thuku.kuria@live.com>
2022-10-25 18:57:09 +0300

    to JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that returns the JSON representation of an object
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    returns a json representation
+    args:
+        my_obj: object to convert
+    """
+
+    return json.dumps(my_obj)
