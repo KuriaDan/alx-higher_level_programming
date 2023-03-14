ab74dcc61fd6d2b40dc230837ca8793bd69b9511
KuriaDan <thuku.kuria@live.com>
2023-03-14 23:33:04 +0300

    classes

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,9 +4,9 @@
  * comstructor takes two arguments w and h.
  */
 class Rectangle {
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
